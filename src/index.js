43ff59ef23e0234bb7be43d83e45fcbb5f776d27
Ali Haydar Demircan <116792531+alidmrcn@users.noreply.github.com>
2023-04-04 19:58:12 +0000

    finalize classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-console.log('This is an inventory managment application for making works easier and reliable.')
-// This app will manage inventory of warehouse and will be able to track inventory items and replacements via Vendors
-
-/*
- */
-
+console.log(
+  'This app will manage inventory of warehouse and will be able to track inventory items and replacements via Vendors'
+)
+class Warehouse {
+  items = []
+  constructor(named, location) {
+    this.named = named
+    this.location = location
+  }
+  addItem(item) {
+    this.items.push(item)
+    console.log(`Item ${this.item} is added to ${this.named} in ${this.location}`) // ???
+  }
+}
 class InventoryItem {
   constructor(modelCode, description, unit, availableQuantity, defectiveQuantity) {
     this.modelCode = modelCode // if there is no model code, it will be replaced with description or auto-generated unique code
@@ -11,38 +19,53 @@ class InventoryItem {
     this.unit = unit // selection from dropdown: piece, meter, kilogram, etc.
     this.availableQuantity = availableQuantity // new or repaired items quantity
     this.defectiveQuantity = defectiveQuantity // defective items quantity
-    // this.inServiceQuantity = inServiceQuantity // items sent to vendor or repair shop for inspenction and replace or repair depends warranty status
-    // this.system = system // related system, selection from dropdown: system list to be created
-    // this.brand = brand // selection from dropdown: vendor list to be created
-    // this.id = id // item number, this property will be replaced with database id when it is connected to database
-    // this.disposedQuantity = disposedQuantity // disposed items quantity, will be functional during later stages of the app development
-    // price = '' // LATER-STAGE, purchased prices with dates
   }
 }
 class Incident {
   constructor(returnDefectiveItems) {
-    this.date = Date.now()
-    this.defectiveItems = []
+    this.date = Date.now() // How to change to date string
+    // this.defectiveItems = []
 
     // ! Create requiredItems property to track required items for replacement
 
     returnDefectiveItems.forEach(item => {
       item.availableQuantity -= 1 // ! if there available quantity
       item.defectiveQuantity += 1
-      this.defectiveItems.push(item)
+      console.log(item.availableQuantity)
+      // this.defectiveItems.push(item)
       // ! if not create requiredItems property and push item to it
     })
   }
 }
+
+const MainWH = new Warehouse('CTP Warehouse', 'Central Plant')
+const TechnicalArea = new Warehouse('Technical Area', 'Terminal')
 
 const Item01 = new InventoryItem('LN1234', '42U Cabin', 'ea', 5, 1)
 const Item02 = new InventoryItem('2960X', '48port Switch', 'ea', 100, 1)
 const Item03 = new InventoryItem('UT1GF', '12 Core', 'm', 5000, 0)
 const Item04 = new InventoryItem('55DXFG', '55" Screen', 'ea', 10, 2)
 
-const incident1 = new Incident([Item01, Item02])
+const Incident01 = new Incident([Item01, Item02, Item03])
+
+MainWH.addItem(Item01)
+MainWH.addItem(Item02)
+TechnicalArea.addItem(Item03)
+TechnicalArea.addItem(Item04)
+
+console.log(Item01)
+console.log('break')
+console.log(MainWH)
+console.log(Incident01)
+console.log(MainWH.location)
 
 // -----------------------------------------------------------------------
+// this.disposedQuantity = disposedQuantity // disposed items quantity, will be functional during later stages of the app development
+// this.inServiceQuantity = inServiceQuantity // items sent to vendor or repair shop for inspenction and replace or repair depends warranty status
+// this.system = system // related system, selection from dropdown: system list to be created
+// this.brand = brand // selection from dropdown: vendor list to be created
+// this.id = id // item number, this property will be replaced with database id when it is connected to database
+// price = '' // LATER-STAGE, purchased prices with dates
 // !! LATER-STAGE warrantyExpireDate = '' // material warranty expire date
 // will be connected to new detailed incident class and will be able to track replacement items and their status
 // will be modified to multiple item class
